f25ef0661d67aa9ced92a32cbfd9e7b5c01e4ad4
cvtsh248 <cvtsh248@gmail.com>
2023-09-08 09:41:41 +0800

    removed bloat

--- obj2thing.py
+++
@@ -1,8 +0,0 @@
-import pywavefront
-scene = pywavefront.Wavefront('teapot.obj')
-print(scene.vertices)
-out = []
-for i in scene.vertices:
-    out.append("Point3D{"+str(i[0])+","+str(i[1])+","+str(i[2])+"}")
-
-print (','.join(map(str, out)))
