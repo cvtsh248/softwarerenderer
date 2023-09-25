11b03afaf6c76ec74b45de3ffb6d4c390afa9a5f
cvtsh248 <cvtsh248@gmail.com>
2023-09-25 08:02:32 +0800

    update run for multiple systems

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-g++ main.cpp 3Dlib/Render3D.cpp 3Dlib/graphicsmath.cpp -std=c++17 -F/Library/Frameworks -framework SDL2
-./a.out
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    g++ main.cpp 3DLib/Render3D.cpp 3DLib/graphicsmath.cpp -std=c++17 -F/Library/Frameworks -framework SDL2
+    ./a.out
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then 
+    g++ main.cpp 3DLib/Render3D.cpp 3DLib/graphicsmath.cpp -w -lSDL2
+    ./a.out
+fi
